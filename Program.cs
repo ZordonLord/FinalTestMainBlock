612b225425074f9052d6e883a35d98f25fad77aa
ZordonLord <zordonlord@mail.ru>
2023-10-07 23:05:37 +1000

    Добавлено решение задачи с 2 вариантами

--- Program.cs
+++ Program.cs
@@ -7,3 +7,52 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+void PrintStringArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+string[] NoMoreThreeCharacters(string[] array) // 1 вариант
+{
+    string temp = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            if (temp == string.Empty) temp += array[i];
+            else temp = temp + "," + array[i];
+        }
+    }
+    string[] filteredArray = temp.Split(',');
+    return filteredArray;
+}
+
+string[] NoMoreThreeCharacters2(string[] array) // 2 вариант
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) size++;
+    string[] filteredArray = new string[size];
+    int k = 0;
+    for (int j = 0; j < array.Length; j++)
+        if (array[j].Length <= 3) filteredArray[k++] = array[j];
+    return filteredArray;
+}
+
+Console.WriteLine("Введите значения через запятую:");
+string[] arrayOfInputValues = Console.ReadLine()!.Replace(" ", "").Split(',');
+PrintStringArray(arrayOfInputValues);
+
+Console.Write(" 1 вариант -> ");
+string[] noMoreThreeCharacters = NoMoreThreeCharacters(arrayOfInputValues);
+PrintStringArray(noMoreThreeCharacters);
+
+Console.Write(" 2 вариант -> ");
+string[] noMoreThreeCharacters2 = NoMoreThreeCharacters2(arrayOfInputValues);
+PrintStringArray(noMoreThreeCharacters2);
